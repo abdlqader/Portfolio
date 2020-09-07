dbb93cc9c812d7c350eeac8874e27e66ee85ce4f
abdulkader <abdulkader_hassan74@hotmail.com>
2020-09-07 19:50:15 +0200

    bootstrap

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,8 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'portfolio/static'),
+    os.path.join(BASE_DIR, 'jobs/static')
+ ]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'

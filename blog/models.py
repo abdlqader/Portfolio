c0546a93a9ff8ce75297bf85e0cbe6d6a936e17b
abdulkader <abdulkader_hassan74@hotmail.com>
2020-09-08 17:13:38 +0200

    almost finished

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-	title = models.CharField(max_length=255)
+	title = models.CharField(max_length=50)
 	pub_date = models.DateTimeField(auto_now=True)
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def pub_date_fancy(self):
+		return self.pub_date.strftime(' %d of %B, %Y')
+
+	def summery(self):
+		result = self.body.split(' ')[:10]
+		return ' '.join(result)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 def home(request):
-	return render(request,'jobs/home.html')
+	Jobs = Job.objects
+	return render(request,'jobs/home.html',{'Jobs':Jobs})
